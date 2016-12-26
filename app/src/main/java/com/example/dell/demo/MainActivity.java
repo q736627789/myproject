3bb564e67ca33e165ea6d8a9790bc951af66f7b2
Your Name <you@example.com>
2016-12-26 14:18:52 +0800

    first commit

---
+++ app/src/main/java/com/example/dell/demo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dell.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"你好",Toast.LENGTH_SHORT).show();
+
+    }
+}
